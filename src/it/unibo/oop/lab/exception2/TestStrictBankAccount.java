774a6e27427a8c17834e01d1ca91199a4f40625b
Emma Leonardi <eleonardi.mobile@gmail.com>
2021-11-03 10:44:09 +0100

    Created tests for Strict Bank Account

--- src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
+++ src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
@@ -1,27 +1,87 @@
 package it.unibo.oop.lab.exception2;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 /**
- * JUnit test to test
- * {@link StrictBankAccount}.
+ * JUnit test to test {@link StrictBankAccount}.
  * 
  */
 public final class TestStrictBankAccount {
 
-    /**
-     * Used to test Exceptions on {@link StrictBankAccount}.
-     */
-    @Test
-    public void testBankOperations() {
-        /*
-         * 1) Creare due StrictBankAccountImpl assegnati a due AccountHolder a
-         * scelta, con ammontare iniziale pari a 10000 e nMaxATMTransactions=10
-         * 
-         * 2) Effetture un numero di operazioni a piacere per verificare che le
-         * eccezioni create vengano lanciate soltanto quando opportuno, cioè in
-         * presenza di un id utente errato, oppure al superamento del numero di
-         * operazioni ATM gratuite.
-         */
-    }
+	/**
+	 * Used to test Exceptions on {@link StrictBankAccount}.
+	 */
+	@Test
+	public void testBankOperations() {
+		/*
+		 * 1) Creare due StrictBankAccountImpl assegnati a due AccountHolder a scelta,
+		 * con ammontare iniziale pari a 10000 e nMaxATMTransactions=10
+		 * 
+		 * 2) Effetture un numero di operazioni a piacere per verificare che le
+		 * eccezioni create vengano lanciate soltanto quando opportuno, cioè in presenza
+		 * di un id utente errato, oppure al superamento del numero di operazioni ATM
+		 * gratuite.
+		 */
+		var Rossi = new AccountHolder("Paolo", "Rossi", 10);
+		var Bianchi = new AccountHolder("Lucia", "Bianchi", 20);
+		var AccRossi = new StrictBankAccount(Rossi.getUserID(), 10000, 10);
+		var AccBianchi = new StrictBankAccount(Bianchi.getUserID(), 10000, 10);
+		
+		assertEquals(AccRossi.getBalance(),10000, 0.5);
+		assertEquals(AccBianchi.getBalance(),10000, 0.5);
+		try {
+		AccRossi.deposit(Rossi.getUserID(), 10);
+		AccBianchi.withdraw(Bianchi.getUserID(), 10);
+		}catch(WrongAccountHolderException |NotEnoughFoundsException e) {
+			fail("Shouldn't have failed");
+		}
+		
+		try {
+			AccBianchi.deposit(Rossi.getUserID(),10);
+			fail();
+		}catch(WrongAccountHolderException e){
+			//Should fail
+		}
+		
+		for(int i=0; i<9; i++) {
+			try {
+			AccRossi.depositFromATM(Rossi.getUserID(), 1);
+			}catch(WrongAccountHolderException| TransactionsOverQuotaException e) {
+				fail("Shouldn't have failed");
+			}
+			try {
+				AccBianchi.withdrawFromATM(Bianchi.getUserID(), 1);
+			}catch(WrongAccountHolderException|TransactionsOverQuotaException|NotEnoughFoundsException e) {
+				fail("Shouldn't have failed");
+			}
+		}
+		
+		try {
+			AccBianchi.depositFromATM(Bianchi.getUserID(), 10);
+			fail();
+		}catch(TransactionsOverQuotaException e) {
+			//Should fail
+		}catch(WrongAccountHolderException e) {
+			fail("Shouldn't have failed");
+		}
+		
+		try {
+			AccRossi.computeManagementFees(Rossi.getUserID());
+		}catch(WrongAccountHolderException|NotEnoughFoundsException e) {
+			fail();
+		}
+		
+		try {
+			AccRossi.withdraw(Rossi.getUserID(), 100000);
+		}catch(NotEnoughFoundsException e) {
+			//Should fail
+		}
+		catch(WrongAccountHolderException e){
+			fail();
+		}
+
+	}
 }
