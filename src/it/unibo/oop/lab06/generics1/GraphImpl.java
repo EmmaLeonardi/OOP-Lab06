252f3e483cc5dbd6659c1157d7cacf586ac77af0
Emma Leonardi <eleonardi.mobile@gmail.com>
2021-11-06 17:29:37 +0100

    Cleaned up Graph Implementation, added comments

--- src/it/unibo/oop/lab06/generics1/GraphImpl.java
+++ src/it/unibo/oop/lab06/generics1/GraphImpl.java
@@ -5,14 +5,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-//import java.util.Iterator;
-//import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-//import java.util.Map.Entry;
 import java.util.Set;
-//import java.util.TreeMap;
-//import java.util.TreeSet;
 import java.util.Queue;
 
 public class GraphImpl<N> implements Graph<N> {
@@ -26,8 +21,8 @@ public class GraphImpl<N> implements Graph<N> {
 
 	public void addEdge(N source, N target) {
 		if (source != null && target != null) {
-			// If the nodes already existed the map is unchanged, otherwise it creates the
-			// nodes
+			// If the nodes already existed the map is unchanged,
+			// otherwise it creates the nodes
 			this.addNode(target);
 			this.addNode(source);
 			graph.get(source).add(target);
@@ -51,49 +46,39 @@ public class GraphImpl<N> implements Graph<N> {
 		var i = graph.keySet().iterator();
 		while (i.hasNext()) {
 			var elem = i.next();
-			parent.put(elem, source);// Initializing the parent vector, every node has source as a parent
+			// Initializing the parent vector, every node has source as a parent
+			parent.put(elem, source);
 		}
-		// Dovrebbe essere solo 1
-		// System.out.println(parent);
-
 		q.add(source);
-		System.out.println("Queue now has:" + q);
 		while (q.isEmpty() == false) {
 			N t = q.remove();
-			System.out.println("Removed t from queue: " + t);
-			System.out.println("Looking at the nodes linked to " + t + ": "+ this.linkedNodes(t));
 			for (N elem : graph.get(t)) {
-				System.out.println("Looking at the nodes linked to " + t + " like, " + elem);
 				if (parent.get(elem) == source && elem != source) {
-					// If the element hasn't already been explored
-					// If the element has source as a parent, t is the new parent
+					// If the element hasn't already been explored OR
+					// If the element isn't source, t is the new parent
 					q.add(elem);
 					parent.put(elem, t);
-					System.out.println(" " + elem + " has now parent " + parent.get(elem));
 				}
-				//System.out.println(parent);
-				//System.out.println("Queue now has:" + q);
-
 			}
-			System.out.println(parent);
-			System.out.println("Queue now has:" + q);
 		}
 
+		/*
+		 * Path is the list that tracks the path to take in the graph,
+		 * using the parent map
+		 */
 		List<N> path = new ArrayList<>();
-		System.out.println(path);
+
+		// First node is target
 		path.add(target);
 		N tmp = parent.get(target);
 		while (tmp != source) {
 			path.add(tmp);
 			tmp = parent.get(tmp);
 		}
+		// Last node is source
 		path.add(source);
-
-		System.out.println("Reversed list");
-		System.out.println(path);
+		// Path is from target to source
 		Collections.reverse(path);
-		System.out.println("Ordered list");
-		System.out.println(path);
 		return path;
 	}
 
