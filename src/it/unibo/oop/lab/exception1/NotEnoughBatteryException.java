7930040039b1a2f955023d2af3a368d557919b2a
Emma Leonardi <eleonardi.mobile@gmail.com>
2021-11-07 13:14:46 +0100

    Added comment explaining why I extended Exception to create Not Enough Battery Exception

--- src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
+++ src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
@@ -3,32 +3,14 @@ package it.unibo.oop.lab.exception1;
 @SuppressWarnings("serial")
 public class NotEnoughBatteryException extends Exception {
 	
+	/*This Exception extends Exception because exceptions are
+	 * predictable problems that can't be fixed*/
+	
 	private final double battery;
 
 	public NotEnoughBatteryException(double battery) {
 		this.battery=battery;
 	}
-
-//	public NotEnoughBatteryException(String message) {
-//		super(message);
-//		// TODO Auto-generated constructor stub
-//	}
-//
-//	public NotEnoughBatteryException(Throwable cause) {
-//		super(cause);
-//		// TODO Auto-generated constructor stub
-//	}
-//
-//	public NotEnoughBatteryException(String message, Throwable cause) {
-//		super(message, cause);
-//		// TODO Auto-generated constructor stub
-//	}
-//
-//	public NotEnoughBatteryException(String message, Throwable cause, boolean enableSuppression,
-//			boolean writableStackTrace) {
-//		super(message, cause, enableSuppression, writableStackTrace);
-//		// TODO Auto-generated constructor stub
-//	}
 	
 	public String toString() {
         return "Cannot move, not enough battery: "+battery+" %";
